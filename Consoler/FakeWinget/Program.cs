1f8400bf98c434837cbd01d2fbfc77e5dec44927
Luis Mascotto <gustavomascotto@gmail.com>
2026-07-04 04:59:17 -0300

    Optional parameters and some console debug

--- Consoler/FakeWinget/Program.cs
+++ Consoler/FakeWinget/Program.cs
@@ -1,6 +1,35 @@
-﻿Console.WriteLine("Fake Winget");
+﻿
+using Microsoft.Extensions.Configuration;
 
+var switchMappings = new Dictionary<string, string>()
+           {
+               { "--waitMs", "WaitMs" },
+               { "--error", "Error" },
+           };
 
-Thread.Sleep(10000);
+string[] extraArgs = [.. args.Skip(3)];
+
+IConfiguration configuration = new ConfigurationBuilder()
+    .AddCommandLine(extraArgs, switchMappings)
+    .Build();
+
+int waitMs = int.Parse(configuration["WaitMs"] ?? "0");
+bool showError = bool.Parse(configuration["Error"] ?? "false");
+
+Console.WriteLine($"Fake-Winget {string.Join(" ", args)}");
+
+if (waitMs <= 0)
+{
+    waitMs = Random.Shared.Next(3000, 6000);
+}
+Console.WriteLine($"Waiting for {waitMs} milliseconds...");
+
+Thread.Sleep(waitMs);
+
+if (showError)
+{
+    Console.WriteLine("Error occurred!");
+    return 1;
+}
 
 return 0;
